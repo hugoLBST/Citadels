5032ddd957f5395c2538b81b35b55a80cc7fbf3f
Thomas Bouchet <Thomas.Bouchet@iutbayonne.univ-pau.fr>
2020-02-14 11:02:12 +0100

    Modification des noms de test en anglais - CityTest

--- src/test/java/com/montaury/citadels/CityTest.java
+++ src/test/java/com/montaury/citadels/CityTest.java
@@ -20,7 +20,7 @@ public class CityTest {
     }
 
     @Test
-    public void test_cout_construction_quartiers(){
+    public void it_should_test_building_district_cost(){
         Possession possession = new Possession(0,HashSet.empty()); // 0 or donc 0 score
         city.buildDistrict(Card.MANOR_5); // +3 score
         city.buildDistrict(Card.WATCHTOWER_2); // +1 score
@@ -29,7 +29,7 @@ public class CityTest {
         assertThat(score).isEqualTo(5);
     }
     @Test
-    public void test_quartiers_5_types(){
+    public void it_should_test_5_district_types_bonus(){
         Possession possession = new Possession(0,HashSet.empty()); // 0 or donc 0 score
         city.buildDistrict(Card.MANOR_5); // +3 score, type : NOBLE
         city.buildDistrict(Card.WATCHTOWER_2); // +1 score, type : MILITARY
@@ -40,7 +40,7 @@ public class CityTest {
         assertThat(score).isEqualTo(13); // Normalement : 10 + Bonus 5 types : 3 -> 13 au total
     }
     @Test
-    public void test_bonus_plus_4_premiere_city_completee(){
+    public void it_should_test_first_completed_city(){
         Possession possession = new Possession(0,HashSet.empty()); // 0 or donc 0 score
         city.buildDistrict(Card.MANOR_5); // +3 score
         city.buildDistrict(Card.WATCHTOWER_2); // +1 score
@@ -54,7 +54,7 @@ public class CityTest {
 
     }
     @Test
-    public void test_bonus_plus_2_deuxieme_city_completee(){
+    public void it_should_test_second_completed_city_bonus(){
         Possession possession1 = new Possession(0,HashSet.empty()); // 0 or donc 0 score
         city.buildDistrict(Card.MANOR_5); // +1 score
         city.buildDistrict(Card.WATCHTOWER_2); // +1 score
@@ -78,7 +78,7 @@ public class CityTest {
     }
 
     @Test
-    public void test_bonus_plus_2_merveilles_Dragon_gate(){
+    public void it_should_test_Dragon_Gate_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or
         city.buildDistrict(Card.DRAGON_GATE); // 6 + 2 points bonus
         int score = city.score(possession);
@@ -86,7 +86,7 @@ public class CityTest {
     }
 
     @Test
-    public void test_bonus_plus_2_merveilles_University(){
+    public void it_should_test_University_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or
         city.buildDistrict(Card.UNIVERSITY); // 6 + 2 points bonus
         int score = city.score(possession);
@@ -94,7 +94,7 @@ public class CityTest {
     }
 
     @Test
-    public void test_bonus_plus_10_merveilles_Treasury(){
+    public void it_should_test_Treasury_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or donc +10 score
         city.buildDistrict(Card.TREASURY); // 5 + 10 points vu que 10 pièces d'or
         int score = city.score(possession);
@@ -102,7 +102,7 @@ public class CityTest {
     }
 
     @Test
-    public void test_bonus_plus_4_merveilles_4_cartes(){
+    public void it_should_test_Map_Room_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or donc +10 score
         city.buildDistrict(Card.DRAGON_GATE); // 6 + 2 points bonus
         city.buildDistrict(Card.UNIVERSITY); // 6 + 2 points bonus
