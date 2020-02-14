c2e66174ae7be92e784a076cd241475f3be8fa88
Thomas Bouchet <Thomas.Bouchet@iutbayonne.univ-pau.fr>
2020-02-14 12:10:14 +0100

    Optimisation des tests - CityTest

--- src/test/java/com/montaury/citadels/CityTest.java
+++ src/test/java/com/montaury/citadels/CityTest.java
@@ -2,6 +2,7 @@ package com.montaury.citadels;
 
 import com.montaury.citadels.district.Card;
 import io.vavr.collection.HashSet;
+import io.vavr.collection.List;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,44 +12,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CityTest {
 
     public Board board;
-    public City city;
+    public static City city;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
        board = new Board();
        city = new City(board);
     }
 
     @Test
     public void it_should_test_building_district_cost(){
-        Possession possession = new Possession(0,HashSet.empty()); // 0 or donc 0 score
-        city.buildDistrict(Card.MANOR_5); // +3 score
-        city.buildDistrict(Card.WATCHTOWER_2); // +1 score
-        city.buildDistrict(Card.TAVERN_5); // +1 score
+        Possession possession = new Possession(0, HashSet.empty()); // 0 or donc 0 score
+        buildDistricts(List.of(Card.MANOR_5, Card.WATCHTOWER_2, Card.TAVERN_5)); // score +1 +1 +3
         int score = city.score(possession);
         assertThat(score).isEqualTo(5);
     }
     @Test
     public void it_should_test_5_district_types_bonus(){
         Possession possession = new Possession(0,HashSet.empty()); // 0 or donc 0 score
-        city.buildDistrict(Card.MANOR_5); // +3 score, type : NOBLE
-        city.buildDistrict(Card.WATCHTOWER_2); // +1 score, type : MILITARY
-        city.buildDistrict(Card.TAVERN_5); // +1 score, type : TRADE
-        city.buildDistrict(Card.CHURCH_3); // +2 score, type : RELIGIOUS
-        city.buildDistrict(Card.KEEP_1); // +3 score, type : SPECIAL
+        buildDistricts(List.of(Card.MANOR_5, Card.WATCHTOWER_2, Card.TAVERN_5, Card.CHURCH_3,Card.KEEP_1)); // score +3 +1 +1 +2 +3
         int score = city.score(possession);
         assertThat(score).isEqualTo(13); // Normalement : 10 + Bonus 5 types : 3 -> 13 au total
     }
     @Test
     public void it_should_test_first_completed_city(){
         Possession possession = new Possession(0,HashSet.empty()); // 0 or donc 0 score
-        city.buildDistrict(Card.MANOR_5); // +3 score
-        city.buildDistrict(Card.WATCHTOWER_2); // +1 score
-        city.buildDistrict(Card.TAVERN_5); // +1 score
-        city.buildDistrict(Card.CHURCH_3); // +2 score
-        city.buildDistrict(Card.TRADING_POST_1); // +2 score
-        city.buildDistrict(Card.TOWN_HALL_1); // +5 score
-        city.buildDistrict(Card.TOWN_HALL_2); // +5 score
+        buildDistricts(List.of(Card.MANOR_5, Card.WATCHTOWER_2, Card.TAVERN_5, Card.CHURCH_3,Card.TRADING_POST_1,Card.TOWN_HALL_1,Card.TOWN_HALL_2)); // score +3 +1 +1 +2 +2 +5 +5
         int score = city.score(possession);
         assertThat(score).isEqualTo(23); // 19 points + 4 points -> 23 au total
 
@@ -56,13 +45,7 @@ public class CityTest {
     @Test
     public void it_should_test_second_completed_city_bonus(){
         Possession possession1 = new Possession(0,HashSet.empty()); // 0 or donc 0 score
-        city.buildDistrict(Card.MANOR_5); // +1 score
-        city.buildDistrict(Card.WATCHTOWER_2); // +1 score
-        city.buildDistrict(Card.TAVERN_5); // +1 score
-        city.buildDistrict(Card.CHURCH_3); // +2 score
-        city.buildDistrict(Card.TRADING_POST_1); // +2 score
-        city.buildDistrict(Card.TOWN_HALL_1); // +5 score
-        city.buildDistrict(Card.TOWN_HALL_2); // +5 score
+        buildDistricts(List.of(Card.MANOR_5, Card.WATCHTOWER_2, Card.TAVERN_5, Card.CHURCH_3,Card.TRADING_POST_1,Card.TOWN_HALL_1,Card.TOWN_HALL_2)); // score +3 +1 +1 +2 +2 +5 +5
         City city2 = new City(board);
         Possession possession2 = new Possession(0,HashSet.empty()); // 0 or donc 0 score
         city2.buildDistrict(Card.MANOR_5); // +3 score
@@ -80,7 +63,8 @@ public class CityTest {
     @Test
     public void it_should_test_Dragon_Gate_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or
-        city.buildDistrict(Card.DRAGON_GATE); // 6 + 2 points bonus
+        buildDistricts(List.of(Card.DRAGON_GATE)); // score 6 +2(bonus)
+
         int score = city.score(possession);
         assertThat(score).isEqualTo(8);
     }
@@ -88,7 +72,7 @@ public class CityTest {
     @Test
     public void it_should_test_University_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or
-        city.buildDistrict(Card.UNIVERSITY); // 6 + 2 points bonus
+        buildDistricts(List.of(Card.UNIVERSITY)); // score 6 +2(bonus)
         int score = city.score(possession);
         assertThat(score).isEqualTo(8);
     }
@@ -96,7 +80,7 @@ public class CityTest {
     @Test
     public void it_should_test_Treasury_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or donc +10 score
-        city.buildDistrict(Card.TREASURY); // 5 + 10 points vu que 10 pièces d'or
+        buildDistricts(List.of(Card.TREASURY)); // score 5 + 10(bonus)
         int score = city.score(possession);
         assertThat(score).isEqualTo(15);
     }
@@ -104,10 +88,7 @@ public class CityTest {
     @Test
     public void it_should_test_Map_Room_wonder_bonus(){
         Possession possession = new Possession(10, HashSet.empty()); // 10 or donc +10 score
-        city.buildDistrict(Card.DRAGON_GATE); // 6 + 2 points bonus
-        city.buildDistrict(Card.UNIVERSITY); // 6 + 2 points bonus
-        city.buildDistrict(Card.TREASURY); // 5 + 10 points vu que 10 pièces d'or
-        city.buildDistrict(Card.MAP_ROOM); // 5 + 4 cartes dans la hand
+        buildDistricts(List.of(Card.DRAGON_GATE,Card.UNIVERSITY,Card.TREASURY,Card.MAP_ROOM)); // score 6 +2(bonus) + 6+2(bonus) + 5 +10(bonus) + 5 + 4(bonus)
         int score = city.score(possession);
         assertThat(score).isEqualTo(40);
     }
@@ -115,13 +96,17 @@ public class CityTest {
     @Test
     public void it_should_test_haunted_city_and_other_districts(){
         Possession possession = new Possession(0,HashSet.empty()); // 0 or donc 0 score
-        city.buildDistrict(Card.TEMPLE_1); // +1 score
-        city.buildDistrict(Card.WATCHTOWER_1); // +1 score
-        city.buildDistrict(Card.TAVERN_3); // +1 score
-        city.buildDistrict(Card.KEEP_1); // +3 score
-        city.buildDistrict(Card.HAUNTED_CITY); // +2 score
+        buildDistricts(List.of(Card.TEMPLE_1,Card.WATCHTOWER_1,Card.TAVERN_3,Card.KEEP_1,Card.HAUNTED_CITY)); // score +1 +1 +1 +3 +2
         int score = city.score(possession);
         assertThat(score).isEqualTo(11); // 8 points + 3 (bonus)
 
+    }
+
+    public static void buildDistricts(List<Card> cardList){
+
+        for(int i = 0; i<cardList.size(); i++)
+        {
+            city.buildDistrict(cardList.get(i));
+        }
     }
 }
